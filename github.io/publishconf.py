d7e806a364aef0872e8eb83713fc2dc57054af78
GreggRoll <adamsg_53@yahoo.com>
2020-09-25 12:13:20 -0400

    set up patch

--- github.io/publishconf.py
+++ github.io/publishconf.py
@@ -10,7 +10,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://'
+DELETE_OUTPUT_DIRECTORY = False
+SITEURL = 'https://greggroll.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
